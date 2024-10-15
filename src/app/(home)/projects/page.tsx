0fc49e034fc25e4f260e218470c8b5e1a35e1115
Kudom <emmanuelkudomagyemang@gmail.com>
2024-10-15 15:41:58 -0400

    unused imports fix

--- src/app/(home)/projects/page.tsx
+++ src/app/(home)/projects/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import React from "react";
 // import project_pic from "@/../public/project.jpeg";
-import Link from "next/link";
-// import { homedir } from "os";
-import { projectlist } from "./data";
+// import Link from "next/link";
+// // import { homedir } from "os";
+// import { projectlist } from "./data";
 import ProjectSection from "../(home)/sections/ProjectSection";
 
 const Projects = () => {
